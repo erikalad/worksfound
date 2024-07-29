d3902aa6bf6a96a11b7fc65550ab70e8e4d3ae87
Erika Ladner <erikaladner@MacBook-Air-de-Erika.local>
2024-07-28 23:51:14 -0300

    logo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="icon" href="/logo.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link
